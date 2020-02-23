e0974efdb84ea2552ff62486e9d60c4acc8df022
kadjiah <kadijahwilson@pursuit.org>
2020-02-23 16:17:55 -0500

    created .gitignore file, the node modules and DS_store won't push to git hub

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var templateRouter = require('./routes/template');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
