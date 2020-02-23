21906244bfd6b646559d29cf85c5ad346221b790
kadjiah <kadijahwilson@pursuit.org>
2020-02-23 12:59:18 -0500

    started template route in the backend

--- Backend/models/Template.js
+++ Backend/models/Template.js
@@ -3,7 +3,7 @@ const db = require('../db/index.js')
 
 getTemplateByid = async (id) => {
     try {
-        const temp = await db.one(`SELECT * from template WHERE id =$/template_id/ `)
+        const temp = await db.one(`SELECT * from template WHERE id =$/template_id/`, [id])
         return temp
     }catch(error){
         res.status(500).json({
