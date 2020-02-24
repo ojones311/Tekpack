69687c4b0174353058901e452e625cdf58acf446
Jenesh <jenesh.napit@gmail.com>
2020-02-24 13:36:07 -0500

    Added search and create new project button with dynamic search functionality

--- frontend/src/components/Overview.jsx
+++ frontend/src/components/Overview.jsx
@@ -2,18 +2,21 @@ import React, { useState } from 'react'
 import ProjectList from './ProjectList'
 
 const Overview = (props) => {
-
     const [projects, setProjects] = useState([
         { id: 1, name: 'T-Shirt' },
         { id: 2, name: 'Jeans' },
-        { id: 3, name: 'Sweater' }
+        { id: 3, name: 'Sweater' },
+        { id: 4, name: 'Gloves' }
     ])
+
+    const [search, setSearch] = useState('')
 
     const getDetailedProject = (id) => {
         props.history.push(`/projects/${id}`)
     }
 
     const deleteProject = (id) => {
+        console.log(`Project ${id} deleted!`)
         const newList = projects.filter(proj => proj.id !== id)
         // Make Request to backend to delete a project
         // If success show new list
@@ -21,40 +24,43 @@ const Overview = (props) => {
         setProjects(newList)
     }
 
+    const searchProjects = (e) => setSearch(e.target.value)
+
     return (
         <div className='project-overview'>
-            <h1>Overview component</h1>
+            <h1 className='center'>Overview component</h1>
+
+            <div className='row valign-wrapper'>
+                {/* <div class="progress">
+                <div class="indeterminate"></div>
+                </div> */}
+                <div className="input-field col s6 pull-s1">
+                    <i className="material-icons prefix">search</i>
+                    <input 
+                        type='text' 
+                        id="icon_prefix" 
+                        className="validate"
+                        onChange={searchProjects}
+                    ></input>
+                    <label for="icon_prefix">Search Projects</label>
+                    {/* <input type='text' placeholder='Search...'/> */}
+                </div>
+                <button className='btn red project-new col s4'>
+                    <i className="material-icons right">add_circle</i>
+                    New Project
+                </button>
+            </div>
+
             <ul className='collection with-header'>
                 <li className='collection-header'>
                     <h3>Projects</h3>
                 </li>
                 <ProjectList
+                    search={search}
                     projects={projects}
                     getDetailedProject={getDetailedProject}
                     deleteProject={deleteProject}
                 />
-                {/* {
-                    projects.map(proj => (
-                        <li className='collection-item'>
-                            <span
-                                className='project-name'
-                                onClick={() => getDetailedProject(proj.id)}>
-                                {proj.id} - {proj.name}
-                            </span>
-                            <span
-                                className='secondary-content'
-                            >
-                                <i
-                                    className='small material-icons blue-text darken-3'
-                                    onClick={() => getDetailedProject(proj.id)}
-                                >
-                                    create
-                                </i>
-                                <i className='small material-icons red-text darken-4'>delete</i>
-                            </span>
-                        </li>
-                    ))
-                } */}
             </ul>
         </div>
     )
