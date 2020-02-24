c108f2ecb6c115b4dd8b5443214d0972e9276d76
Jenesh <jenesh.napit@gmail.com>
2020-02-24 17:35:57 -0500

    Testing image upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,20 @@ import Home from './containers/Home'
 import About from './containers/About'
 import Projects from './containers/Projects'
 import 'materialize-css'
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAn2fkKKNOF_yApAfxYeTVCvJ8eWb64dS0",
+  authDomain: "tekpack-12321.firebaseapp.com",
+  databaseURL: "https://tekpack-12321.firebaseio.com",
+  projectId: "tekpack-12321",
+  storageBucket: "tekpack-12321.appspot.com",
+  messagingSenderId: "142712573474",
+  appId: "1:142712573474:web:f50d1b5d97998b590c047b",
+  measurementId: "G-WRLTKN3Z5C"
+}
+
+firebase.initializeApp(config)
 
 class App extends Component {
   state = {
