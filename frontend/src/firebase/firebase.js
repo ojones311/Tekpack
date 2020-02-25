c2e0d4fbf31edb4d8d7ab62f12adbc37ffd3ff56
Jenesh <jenesh.napit@gmail.com>
2020-02-25 15:55:13 -0500

    Updated config to use env variables but for some reason its not working

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -1,16 +1,24 @@
 import * as firebase from 'firebase'
 require('dotenv').config()
+// import dotenv from 'dotenv'
+// import './env'
+
+// dotenv.config()
+
+console.log(process.env)
 
 const config = {
-    apiKey: process.env.FIREBASE_API,
-    authDomain: "hackathon-tekpack.firebaseapp.com",
-    databaseURL: "https://hackathon-tekpack.firebaseio.com",
-    projectId: "hackathon-tekpack",
-    storageBucket: "hackathon-tekpack.appspot.com",
-    messagingSenderId: "441214947070",
-    appId: "1:441214947070:web:49b71e28694261185fdfaf",
-    measurementId: "G-9F1WKHMM8E"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 }
+
+console.log(config)
 
 firebase.initializeApp(config)
 
@@ -20,3 +28,4 @@ export {
     storage,
     firebase as default
 }
+
