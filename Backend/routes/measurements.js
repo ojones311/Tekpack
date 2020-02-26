d759bd36dfb1cd783f41b35d89050587c6ea6daf
Jenesh <jenesh.napit@gmail.com>
2020-02-26 14:27:27 -0500

    Commented out random measurements variable

--- Backend/routes/measurements.js
+++ Backend/routes/measurements.js
@@ -2,30 +2,30 @@ var express = require('express');
 var router = express.Router();
 const Measurements = require('../models/Measurements')
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.get('/project/:project_id', async(req, res, next) => {
-    const {project_id} = req.params
-    try{
+router.get('/project/:project_id', async (req, res, next) => {
+    const { project_id } = req.params
+    try {
         const allMeasurementsByProjectId = await Measurements.getAllMeasurementsByProjectId(project_id)
         res.json({
             payload: allMeasurementsByProjectId,
             msg: 'Getting all measurements by project id',
             err: false
         })
-    }catch(error){
+    } catch (error) {
         console.log('mod error', error)
-            res.status(500).json({
-                payload: null,
-                msg: error,
-                err: true
+        res.status(500).json({
+            payload: null,
+            msg: error,
+            err: true
         })
     }
 })
 router.post('/form', async (req, res, next) => {
-    const formData = {...req.body}
+    const formData = { ...req.body }
     delete formData.measurement_id
     delete formData.projects_id
 
@@ -36,20 +36,20 @@ router.post('/form', async (req, res, next) => {
 
 
 
-    const {measurement_id, hps, cf, cb, ss, projects_id} = req.body
+    const { measurement_id, hps, cf, cb, ss, projects_id } = req.body
     const measurements = {
         measurement_id,
         hps,
         cf,
-        cb, 
-        ss, 
-        projects_id  
+        cb,
+        ss,
+        projects_id
     }
-    const newMeasurements = 
+    // const newMeasurements = 
 })
 
 
-router.put('edit/measurement', async(req,res,next) => {
+router.put('edit/measurement', async (req, res, next) => {
     // const {}
 })
 
