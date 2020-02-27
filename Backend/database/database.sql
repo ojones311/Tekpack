991e7513607fbb335efe56ba0cc1bbcd4d088cf9
kadjiah <kadijahwilson@pursuit.org>
2020-02-27 17:15:35 -0500

    Created default templates table

--- Backend/database/database.sql
+++ Backend/database/database.sql
@@ -76,7 +76,11 @@ CREATE TABLE measurement
     projects_id INT REFERENCES projects(projects_id)
 );
 
-
+CREATE TABLE defaultTemplates (
+deftemplate_id SERIAL PRIMARY KEY, 
+img_name VARCHAR,
+ image VARCHAR
+);
 
 INSERT INTO users
     (name, email, number, password)
@@ -123,4 +127,10 @@ VALUES
     (5, 9, 10, 3, 4),
     (10, 3, 5, 14, 5);
 
+INSERT INTO defaultTemplates (deftemplate_id, img_name, image)
+VALUES
+(1,  'T-shirt',  'https://pluspng.com/img-png/tshirt-png-outline-blank-t-shirt-outline-1663530-1421.jpg'),
+(2, 'Hoodie',  'https://i.ya-webdesign.com/images/hoodie-template-png-13.png' ),
+(3,'Jeans', 'https://i.pinimg.com/originals/7e/e7/81/7ee78144307504c7de8c4b50255a0ca8.png'),
+(4, 'Socks',  'https://media.istockphoto.com/vectors/sock-template-vector-id512011001?k=6&m=512011001&s=612x612&w=0&h=tNkOx3mG7dRj5X1rKB46mYu77ehGHtH0rsNA1GieDmk=');
 
