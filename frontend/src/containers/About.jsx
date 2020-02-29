c8479bcc1c68c29a8e13c4c07cb79f7f02eacfe5
kadjiah <kadijahwilson@pursuit.org>
2020-02-29 13:40:32 -0500

    updated about page

--- frontend/src/containers/About.jsx
+++ frontend/src/containers/About.jsx
@@ -5,35 +5,41 @@ const About = () => {
     return (
         <div>
             <h1>About Page</h1>
-            <p>What if I told you to make 2,000 gallons of water to make the jeans you are wearing right now, you wouldn’t believe it.
+            <p>It takes 2,000 gallons of water to make the jeans you are wearing right now, you wouldn’t believe it.
                 Nobody ever really thinks what it took to make a piece of clothing we just like it and buy it. </p>
 
             <p>
-                The big problem in the fashion world today is that textile dying is the second largest polluter of water globally and
-                majority of these textiles are ended up in landfills or burned because of:
+                One problem in the fashion world is that textile dying is the second largest polluter of
+                water globally and 73% of these textiles end up in landfills or burned because of sample exchanges between designer
+                 and factory supplier. 
+                 <br/>
+                 Here's why:
                 <or>
-                    <li>sample exchanges between designer and factory supplier</li>
-                    <li>design && specification inaccuracy</li>
-                    <li>lost mail and emails</li>
-                    <li>miscommunications between the two parties</li>
-                    <li>hard to manage hard copy files for each product</li>
+                    <li>Design and specification inaccuracy</li>
+                    <li>Lost samples and specification sheets in mail and emails</li>
+                    <li>Miscommunications between the two parties</li>
+                    <li>Hard to manage hard copy files for each product</li>
                 </or>
             </p>
             <p>
-            Tekpack is your one stop shop to manage and keep track of all product specifications and its related content seamlessly. The whole process will cut down on waste in the supply chain and prevent costly mistakes in production. 
-            We’ve created a simple user interface that helps designers troubleshoot before any fabric is cut. These can be made with real time updates and reduce human error to create a better communication flow between designer and factory. 
-            A user can also upload their own design and changes, they can upload a modified sketch that will be updated instantly for all parties associated on the project. Also for designers we’ve added an template option where they can choose from one of our basic templates. Further increasing productivity when working on designs with similar specs.
-
+                Tekpack is your one stop shop to manage and keep track of all product specifications and its related content seamlessly.
+                 The whole process will cut down on waste in the supply chain and prevent costly mistakes in production. We’ve created a simple 
+                 user interface that helps designers troubleshoot before any fabric is cut. These can be made with real time updates and reduce 
+                 human error to create a better communication flow between designer and factory. A user can also upload their own design
+                  and changes, they can upload a modified sketch that will be updated instantly for all parties associated on the project. 
+                  Also for designers we’ve added an template option where they can choose from one of our basic templates. Further increasing 
+                  productivity when working on designs with similar specs.
+    
             </p>
             <p>
                 For Future features
                 <or>
                     <li>live changes</li>
                     <li>Designers to be able to send factories their url link. The supplier will have to sign up under the supplier account to
-                        access the designers projects. Supplier will not be allowed to edit images or measurements but they will be able to comment 
+                        access the designers projects. Supplier will not be allowed to edit images or measurements but they will be able to comment
                         on designers projects.</li>
                     <li> Designers sketches will be 3D and dynamic where once a measurement is inputed the image would display the input in real time.</li>
-                    <li>Add an Export option where suppliers are able to export the spec sheet as a PDF or just the measurements as an excel 
+                    <li>Add an Export option where suppliers are able to export the spec sheet as a PDF or just the measurements as an excel
                         sheet for printing purposes.</li>
                     <li>QR Code to get product specs instantly</li>
                 </or>
